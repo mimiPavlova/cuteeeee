54ae2fa7b399a6f8c781011cd7f1c08f5b062c3f
Мария Павлова <ivana.prego@gmail.com>
2024-07-20 22:26:29 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // ASCII Art of a Cat and Dog 
+        Console.ForegroundColor = ConsoleColor.Green;
+        string asciiArt = @"
+  /\_/\  
+ ( o.o ) 
+  > ^ <
+
+   ^
+  / \__
+ (    @\___
+ /         O
+/   (_____/
+/_____/   
+
+";
+
+        Console.Clear(); // Clear the console window
+        Console.WriteLine(asciiArt); // Print ASCII Art
+        //Console.ReadLine(); // Wait for user input
+    }
+}
